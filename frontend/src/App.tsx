247042a273d706644cfb681f19dcd37926937d3f
flyri0 <contato.flyrio.dev@gmail.com>
2026-03-25 21:44:07 -0300

    frontend: add a theme provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,11 @@
+import { ThemeProvider } from "./components/theme-provider"
+
 export function App() {
-  return <h1>Hello, Wails!</h1>
+  return (
+    <ThemeProvider defaultTheme="dark">
+      <h1>Hello, Wails!</h1>
+    </ThemeProvider>
+  )
 }
 
 export default App
