b8acd2254f493b7ec38a52cb2dc3099b5681e94b
flyri0 <franciscocandidolyrio@gmail.com>
2026-04-03 19:11:31 -0300

    add memory based navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,20 +5,23 @@ import WelcomePage from "./pages/welcome"
 import PickTargetPage from "./pages/pick-target"
 import FinishedPage from "./pages/finished"
 import InstallPage from "./pages/install"
+import { memoryLocation } from "wouter/memory-location"
 
 export default function App() {
+  const { hook } = memoryLocation({ path: "/" })
+
   return (
-    <Router>
-      <ThemeProvider defaultTheme="dark">
-        <TooltipProvider>
+    <ThemeProvider defaultTheme="dark">
+      <TooltipProvider>
+        <Router hook={hook}>
           <Switch>
             <Route path="/" component={WelcomePage} />
             <Route path="/pick-target" component={PickTargetPage} />
             <Route path="/install" component={InstallPage} />
             <Route path="/finished" component={FinishedPage} />
           </Switch>
-        </TooltipProvider>
-      </ThemeProvider>
-    </Router>
+        </Router>
+      </TooltipProvider>
+    </ThemeProvider>
   )
 }
